ae98032eab895deb3516ae40d4affdde5f5c26f5
Pankaj Kumar <87565770+panku121@users.noreply.github.com>
2024-05-06 17:59:32 +0000

    Counter Close

--- js/main.js
+++ js/main.js
@@ -175,31 +175,30 @@
 
 	
 
-	var counter = function() {
+	// var counter = function() {
 		
-		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
+	// 	$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
 
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+	// 		if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
 
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
+	// 			var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+	// 			$('.number').each(function(){
+	// 				var $this = $(this),
+	// 					num = $this.data('number');
+	// 				$this.animateNumber(
+	// 				  {
+	// 				    number: num,
+	// 				    numberStep: comma_separator_number_step
+	// 				  }, 7000
+	// 				);
+	// 			});
 				
-			}
+	// 		}
 
-		} , { offset: '95%' } );
+	// 	} , { offset: '95%' } );
 
-	}
-	counter();
+	// }
+	// counter();
 
 
 	var contentWayPoint = function() {
